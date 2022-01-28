0d43ff85e900022b3de477180a8489512f3f974f
norikokono <55174696+norikokono@users.noreply.github.com>
2022-01-27 23:55:50 -0800

    added Loading to Mars.js

--- src/components/Mars/Mars.js
+++ src/components/Mars/Mars.js
@@ -7,11 +7,15 @@ const apiKey = process.env.REACT_APP_NASA_KEY;
 
 const Mars = () => {
   const [data, setData] = useState([]);
+  // https://stackoverflow.com/questions/66444067/react-display-loading-animation-for-api-call-duration
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleFetch = async () => {
+    setIsLoading(true);
     const response = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=${apiKey}`, {
       method: "GET",
-    })
+    });
+    setIsLoading(false);
     const fetchData = await response.json()
     const fetchData2 = fetchData.photos.slice(0,10)
     setData(fetchData2.map(arrayItem => {
@@ -24,6 +28,8 @@ const Mars = () => {
       <NavBar />
       <div className="mars-app">
         <h1>Mars rover images</h1>
+        {/* https://stackoverflow.com/questions/66444067/react-display-loading-animation-for-api-call-duration */}
+        {isLoading ? <p className="mars-loading">Loading...</p> : null}
         <button onClick={handleFetch} className="mars-button">Upload Images</button>
       
         <ol>
